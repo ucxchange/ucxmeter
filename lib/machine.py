79cfc14bb5989cfffbc03809191870ba2ee096df
Rico Rojas <rico@ucxchange.com>
2015-08-25 16:30:54 -0500

    machine.py work

--- lib/machine.py
+++ lib/machine.py
@@ -1,6 +1,9 @@
 import requests
 import json
 import psutil
+from cpuinfo import cpuinfo
+from netifaces import interfaces, ifaddresses, AF_INET
+import uuid
 
 headers = {'content-type': 'application/json'}
 oauth_token = "30a62bf3a34104c882eaa47655e99fa6b81ea1fd3428fa5f5e43b74b4b0a7729"
@@ -9,69 +12,80 @@ class machine(object):
     def __init__(self):
         self.create_machine_uri = ""
 
-    # def machine_exist(self, id):
-    #         URI = "https://console.6fusion.com:443/api/v2/"
-    #         URI += "organizations/%s/infrastructures/%s/machines.json?" % (org_id, infr_id)
-    #         URI += "access_token=%s" % oauth_token
-    #         req = requests.get(URI)
-    #         reqInfo = json.loads(req.text)
-    #         machines = reqInfo['embedded']['machines']
-    #         for inf in machines:
-    #             if inf['remote_id'] == mach_name:
-    #                 infraId = infrastructures['remote_id']
-    #                 return str(infraId)
-    #         return 0
-    #         machine_data = json.dumps(machine_details, ensure_ascii=True)
-    #         machinePost = requests.post(URI, data=machine_data, headers=headers)
-    #         reqInfo = json.loads(machinePost.text)
-    #         machineInfo = reqInfo['remote_id']
-    #             return machineInfo
+    def machine_exist(self, id):
+        return 0
 
-    def create_machine(self, org_id, infr_id):
-        uuid = self.create_uuid_for_machine_name()
+    def get_mem(self):
+        mem_info = psutil.virtual_memory()
+        self.total_memory = mem_info[0]
+
+        i = 1
+
+    def get_cpu_info(self):
+        cInfo = cpuinfo.get_cpu_info()
+        self.cpu_speed = cInfo['hz_actual_raw'][0]
+        self.cores = cInfo['count']
+        self.total_cpu_speed = self.cpu_speed * self.cores
+
+        i = 1
+
+    def get_nics(self):
+        self.nics = []
+        for interface in interfaces():
+            try:
+                for link in ifaddresses(interface)[AF_INET]:
+                    if '127.0.0.1' == link['addr']:
+                        continue
+                    self.nics.append({"name": interface,
+                                      "link": link['addr'],
+                                      "kind": 0,
+                                      "mac_address": "aa:aa:aa:aa:aa:aa"})
+            except:
+                pass
+
+        i = 1
+
+    def get_disks(self):
+        self.disk_info = []
+        disks = psutil.disk_partitions()
+        for disk in disks:
+            total_disk_size = psutil.disk_usage(disk[1])[0]
+            self.disk_info.append({"device": disk[0],
+                                   "maximum_size_bytes": total_disk_size,
+                                   "type": "DISK"})
+
+        i = 1
+
+    def create_machine(self):
+        server = "server_" + str(uuid.uuid4())
+
         machine_details = {
-            "name": "new",
-            "virtual_name": "suckIt",
-            "cpu_count": 75,
+            "name": "%s" % server,
+            "virtual_name": server,
+            "cpu_count": self.cores,
             "cpu_speed_mhz": 3000,
             "maximum_memory_bytes": psutil.virtual_memory().total,
             "tags": "",
-            "status": "",
-            "disks": [
-                {
-                    "uuid": "",
-                    "name": "bootDisk",
-                    "maximum_size_bytes": 324235,
-                    "type": ""
-                }
-            ],
-            "nics": [
-                {
-                    "uuid": "",
-                    "name": "eth0",
-                    "kind": 0,
-                    "ip_address": "127.0.0.1",
-                    "mac_address": "aa:aa:aa:aa:aa:aa"
-                }
-            ]
+            "status": "poweredOn",
+            "disks": self.disk_info,
+            "nics": self.nics
         }
 
-        # if uuid != 0:
-        #     print("Machine exists. Moving onto add measurements/readings.")
-        # else:
-        try:
-            URI = "https://console.6fusion.com:443/api/v2/"
-            URI += "organizations/%s/infrastructures/%s/machines.json?" % (org_id, infr_id)
-            URI += "access_token=%s" % oauth_token
-            machine_data = json.dumps(machine_details, ensure_ascii=True)
-            machinePost = requests.post(URI, data=machine_data, headers=headers)
-            reqInfo = json.loads(machinePost.text)
-            machineInfo = reqInfo['remote_id']
-            return machineInfo
-        except Exception as e:
-            print('ERROR: ' + str(e))
-            raise Exception('Infrastructure creation failed.  Halting execution')
+        print machine_details
 
-    def create_uuid_for_machine_name(self):
-        return "fdkjfkdjfd"  # combo of base64 for company name, "-", machine identifier - mac perhaps
+        i = 1
 
+
+def main():
+    machineInfo = machine()
+    machineInfo.get_cpu_info()
+    machineInfo.get_mem()
+    machineInfo.get_nics()
+    machineInfo.get_disks()
+
+    machineInfo.create_machine()
+
+    i = 1
+
+if __name__ == "__main__":
+    main()
